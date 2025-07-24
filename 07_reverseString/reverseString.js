f53549a42257ee1ef33658c2e7a911c84c9d97d0
Tafrija <tafrija123@gmail.com>
2025-07-24 10:43:23 +0300

    splitted , reversed and joined string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
 const reverseString = function() {
+const str = " hello there";
+
+ return str.split("").reverse().join("");
 
 };
-
+console.log(reverseString());
 // Do not edit below this line
 module.exports = reverseString;
