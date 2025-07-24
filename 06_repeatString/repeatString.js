bef624182b1920ea3c7b45fc4e3a8976e5ef0a68
Tafrija <tafrija123@gmail.com>
2025-07-24 10:12:51 +0300

    String Repeated without built-in function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let myString = '';
+    for(num = 0 ; num < 3; num++){
+        string+="hey"
+    }
+    
+return myString;
 };
-
+console.log(repeatString());
 // Do not edit below this line
 module.exports = repeatString;
